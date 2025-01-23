98fef66e4fcabffb9954cf0f482ab0f8886b519b
Seunghyo <shane.chang@parksystems.com>
2025-01-23 11:18:31 +0900

    Update 1.5.5 브로드캐스트 추가

---
+++ 0_Ch1/5_1_5_1.py
@@ -0,0 +1,14 @@
+
+# 1.5.5 브로드캐스트 p.39
+# 4번 파일에서 보았듯 브로드 캐스트는 행렬 크기가 서로 달라도 
+# 어느정도 연산이 가능하도록 도와줍니다. 
+import numpy as np
+
+A = np.array([[1,2],[3,4]])
+B = np.array([10,20])
+print("A is")
+print(A)
+print("B is")
+print(B)
+print("A*B is:")
+print(A*B)
