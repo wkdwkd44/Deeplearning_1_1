50ff24eac84e734eeeb5aa5bdb90f99cb523ce5c
Seunghyo <shane.chang@parksystems.com>
2025-01-23 09:45:01 +0900

    Update 1.3.2 자료형 내용 추가

--- 0_Ch1/0_1_3_1_산술연산.py
+++ 0_Ch1/0_1_3_1_산술연산.py
@@ -5,3 +5,13 @@ print("1+2:",1+2) # "1+2:3" 출력
 print("4*5:",4*5) # "4*5:20" 출력
 print("7/5:",7/5) # "7/5:1.4" 출력
 print("3**2:",3**2) # "3**2:9" 출력
+
+# 1.3.2 자료형 p.29
+# python 에서는 자료형을 지정하지 않아도 됩니다.
+# 자료형을 지정하지 않아도 자동으로 인식합니다.
+# 해당 자료형을 확인 할 수 있습니다. 
+
+print("10 class is",type(10)) # <class 'int'> 출력
+print("2.718 class is",type(2.718)) # <class 'float'> 출력
+print(" \"Hello\" class is",type("Hello")) # <class 'str'> 출력
+
